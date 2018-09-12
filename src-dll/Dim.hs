24076910567c8c4a62c9554eeb7b13b1b41b5a32
stla <laurent_step@yahoo.fr>
2018-09-13 00:55:12 +0200

    fidVertex6

--- src-dll/Dim.hs
+++ src-dll/Dim.hs
@@ -636,3 +636,174 @@ loop n = do
   x <- go 0 0
   let x' = VS.singleton x
   return $ VS.unsafeToSEXP x'
+
+--
+foreign export ccall fidVertex6 :: SEXP s 'R.Real -> SEXP s 'R.Int
+                                -> SEXP s 'R.Int -> SEXP s 'R.Real
+                                -> SEXP s 'R.Real -> SEXP s 'R.Real
+                                -> SEXP s 'R.Int -> SEXP s 'R.Int
+                                -> SEXP s 'R.Int -> IO (SEXP s R.Vector)
+fidVertex6 :: SEXP s 'R.Real -> SEXP s 'R.Int
+           -> SEXP s 'R.Int -> SEXP s 'R.Real
+           -> SEXP s 'R.Real -> SEXP s 'R.Real
+           -> SEXP s 'R.Int -> SEXP s 'R.Int -> SEXP s 'R.Int
+           -> IO (SEXP s R.Vector)
+fidVertex6 _vt1 _p _cc1 _vtsum _u _v _dim _n _k = do
+  let p = (VS.!) (VS.unsafeFromSEXP _p) 0
+      p' = fromIntegral p :: Int
+      range_p' = [0 .. p'-1]
+      u = (VS.!) (VS.unsafeFromSEXP _u) 0
+      v = (VS.!) (VS.unsafeFromSEXP _v) 0
+      dim = (VS.!) (VS.unsafeFromSEXP _dim) 0
+      dim' = fromIntegral dim :: Int
+      range_dim = [0 .. dim - 1]
+      range_dim' = [0 .. dim' - 1]
+      n = (VS.!) (VS.unsafeFromSEXP _n) 0
+      k = (VS.!) (VS.unsafeFromSEXP _k) 0
+      vtsum = VS.unsafeFromSEXP _vtsum
+      cc1 = VS.unsafeFromSEXP _cc1
+      vt1 = VS.unsafeFromSEXP _vt1
+      l = VS.foldl' (\i b -> i + fromEnum (b < v)) 0 vtsum
+  cctemp <- newIORef SV.empty :: IO (IORef (SV.Vector Int32))
+  vert <- newIORef 0 :: IO (IORef Int32)
+  vttemp <- newIORef SV.empty :: IO (IORef (SV.Vector Double))
+  when (l>0 && l < p') $ do
+    let checkl_list = [j | j <- range_p', (vtsum VS.! j) < v]
+        checkl = UV.fromList checkl_list
+        range_l = [0 .. l-1]
+        cols = V.fromList (map (\j ->
+                                [cc1 VS.! indexMatrix i (checkl UV.! j) dim' |
+                                  i <- range_dim'])
+                               range_l)
+        assocs = [((i,j),1) | j <- range_l, i <- cols V.! j] ++
+                 [((i,j),0) |
+                  j <- range_l, i <- range_dim, i `notElem` cols V.! j]
+        int = UA.array ((0,0),(2*n-1,l-1)) assocs :: UArray (Int32, Int) Int32
+        whichl_list = [j | j <- range_p', (vtsum VS.! j) >= v]
+        vtsum_l = UV.fromList [vtsum VS.! j | j <- whichl_list]
+        whichl = UV.fromList whichl_list
+        vtsum_ll = UV.fromList [vtsum VS.! j | j <- checkl_list]
+        go :: Int -> IO ()
+        go ii
+          | ii == p'-l = return ()
+          | otherwise = do
+            let iii = whichl UV.! ii
+                int2 = map (\j -> [int UA.! (i,j) |
+                                   i <- [cc1 VS.! ij2k r iii dim' |
+                                         r <- range_dim']]) range_l
+                colSums = map sum int2
+                use = elemIndices (dim-1) colSums
+                len_use = length use
+            when (len_use > 0) $ do
+              modifyIORef vert (+ fromIntegral len_use)
+              let vtsum_l_ii = vtsum_l UV.! ii
+                  vt1_l_ii = SV.fromList [vt1 VS.! ij2k i iii dim' | i <- range_dim']
+                  inner :: Int -> SV.Vector Int32 -> SV.Vector Double
+                        -> (SV.Vector Int32, SV.Vector Double)
+                  inner dd !cc !vt
+                    | dd == len_use = (cc,vt)
+                    | otherwise = inner (dd+1) (cc SV.++ ccnew) (vt SV.++ vtnew)
+                      where
+                        ddi = use !! dd
+                        ddii = checkl UV.! ddi
+                        inter = [cc1 VS.! indexMatrix k iii dim' |
+                                 k <- elemIndices 1 (int2 !! ddi)]
+                        ccnew = SV.snoc (SV.fromList inter) (k+n)
+                        lambda = (v - vtsum_l_ii) /
+                                 ((vtsum_ll UV.! ddi) - vtsum_l_ii)
+                        vt1_ll_dd = SV.fromList [vt1 VS.! ij2k i ddii dim' |
+                                                 i <- range_dim']
+                        vtnew = SV.zipWith (\a b -> lambda*a + (1-lambda)*b)
+                                           vt1_ll_dd vt1_l_ii
+              let (ccnew, vtnew) = inner 0 SV.empty SV.empty
+              modifyIORef cctemp (SV.++ ccnew)
+              modifyIORef vttemp (SV.++ vtnew)
+            go (ii+1)
+    go 0
+  ----
+  let lu = VS.foldl' (\i b -> i + fromEnum (b > u)) 0 vtsum
+  when (lu > 0 && lu < p') $ do
+    let checku_list = [j | j <- range_p', (vtsum VS.! j) > u]
+        checku = UV.fromList checku_list
+        range_lu = [0 .. lu-1]
+        columns = V.fromList (map (\j ->
+                                  [cc1 VS.! indexMatrix i (checku UV.! j) dim' |
+                                   i <- range_dim'])
+                                  range_lu)
+        assocs = [((i,j),1) | j <- range_lu, i <- columns V.! j] ++
+                 [((i,j),0) |
+                    j <- range_lu, i <- range_dim, i `notElem` columns V.! j]
+        int = UA.array ((0,0),(2*n-1,lu-1)) assocs :: UArray (Int32, Int) Int32
+        whichu_list = [j | j <- range_p', (vtsum VS.! j) <= u]
+        vtsum_u = UV.fromList [vtsum VS.! j | j <- whichu_list]
+        vtsum_uu = UV.fromList [vtsum VS.! j | j <- checku_list]
+        whichu = UV.fromList whichu_list
+        go :: Int -> IO ()
+        go ii | ii == p'-lu = return ()
+              | otherwise = do
+                let whichu_ii = whichu UV.! ii
+                    range_for_int2 = [cc1 VS.! indexMatrix r whichu_ii dim' |
+                                      r <- range_dim']
+                    int2 = map (\j -> [int UA.! (i,j) |
+                                       i <- range_for_int2]) range_lu
+                    colSums = map sum int2
+                    use = elemIndices (dim-1) colSums
+                    len_use = length use
+                when (len_use > 0) $ do
+                  modifyIORef vert (+ fromIntegral len_use)
+                  let vtsum_u_ii = vtsum_u UV.! ii
+                      vt1_u_ii = SV.fromList
+                                  [vt1 VS.! indexMatrix i whichu_ii dim' |
+                                   i <- range_dim']
+                      inner :: Int -> IO ()
+                      inner dd | dd == len_use = return ()
+                               | otherwise = do
+                                 let ddi = use !! dd
+                                     ddii = checku UV.! ddi
+                                     inter = [cc1 VS.! ij2k k whichu_ii dim' |
+                                              k <- elemIndices 1 (int2 !! ddi)]
+                                     ccnew = SV.snoc (SV.fromList inter) k
+                                 modifyIORef cctemp (SV.++ ccnew)
+                                 -- modifyIORef vert (+1)
+                                 let lambda = (u - vtsum_u_ii) /
+                                              ((vtsum_uu UV.! ddi) - vtsum_u_ii)
+                                     vt1_uu_dd = SV.fromList
+                                                    [vt1 VS.! ij2k i ddii dim' |
+                                                     i <- range_dim']
+                                     vtnew = SV.zipWith
+                                             (\a b -> lambda*a + (1-lambda)*b)
+                                             vt1_uu_dd vt1_u_ii
+                                 modifyIORef vttemp (SV.++ vtnew)
+                                 inner (dd+1)
+                  inner 0
+                go (ii+1)
+    go 0
+  --
+  let which_both = UV.fromList
+                   [j | j <- range_p', inInterval (vtsum VS.! j) (v,u)]
+      lb = UV.length which_both
+  when (lb > 0) $ do
+    modifyIORef vert (+ fromIntegral lb)
+    let go :: Int -> SV.Vector Int32 -> SV.Vector Double
+           -> (SV.Vector Int32, SV.Vector Double)
+        go ii !cc !vt
+          | ii == lb = (cc, vt)
+          | otherwise =
+            go (ii+1) (cc SV.++ SV.fromList cc1_ii) (vt SV.++ SV.fromList vt1_ii)
+            where
+              iii = which_both UV.! ii
+              cc1_ii = [cc1 VS.! indexMatrix i iii dim' | i <- range_dim']
+              vt1_ii = [vt1 VS.! indexMatrix i iii dim' | i <- range_dim']
+        (ccnew, vtnew) = go 0 SV.empty SV.empty
+    modifyIORef cctemp (SV.++ ccnew)
+    modifyIORef vttemp (SV.++ vtnew)
+  --
+  out_cctemp <- readIORef cctemp
+  out_vert <- readIORef vert
+  out_vttemp <- readIORef vttemp
+  out_vert' <- intToSEXP [out_vert]
+  out_vttemp' <- realToSEXP' out_vttemp
+  out_cctemp' <- intToSEXP' out_cctemp
+  setDim out_vttemp' dim out_vert
+  setDim out_cctemp' dim out_vert
+  return $ vectorAppend (vectorAppend (toList out_vert') out_vttemp') out_cctemp'
